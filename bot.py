50afb8b6a109e00259098771772aed829691279e
Timur Nikolaev <timplay42@mail.ru>
2023-10-23 20:54:18 +0300

    Создал шаблон бота

---
+++ bot.py
@@ -0,0 +1,8 @@
+from config_data.config import load_config
+
+config = load_config('.env')
+
+bot_token = config.tg_bot.token 
+superadmin = config.tg_bot.admin_ids[0]
+
+print(bot_token, superadmin)
